9544bebecbe4856046df92f3761c9b4dea585c0f
samylly <samyl@DESKTOP-TRVTAH5>
2022-08-14 22:03:32 -0300

    adicao da camada Domain (dominio)

---
+++ Domain/Interface/Generics/IGeneric.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Interface.Generics
+{
+    public interface IGeneric<T> where T : class
+    {
+        Task Add(T objeto);
+
+        Task Update(T objeto);
+
+        Task Delete(T objeto); 
+
+        Task<T> GetEntityById(int Id);
+        
+        Task<List<T>> List();
+    }
+}
---
+++ Domain/Services/ServiceMessage.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Interface;
+using Domain.Interface.InterfaceService;
+
+namespace Domain.Services
+{
+    public class ServiceMessage : IServiceMessege
+    {
+        private readonly IMessage _IMessage;
+
+        public ServiceMessage(IMessage IMessage)
+        {
+            _IMessage = IMessage;
+        }
+    }
+}
