66055ec967b161882955d02235f4cb5fb7555440
samylly <samyl@DESKTOP-TRVTAH5>
2022-08-14 21:22:51 -0300

    adicao das entidades do sistema

---
+++ Entites/Entites/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Entites.Enums;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entites.Entites
+{
+    public class ApplicationUser: IdentityUser
+    {
+        [Column("USR_Cpf")]
+        public String CPF { get; set; }
+
+        [Column("USR¨_tipo")]
+        public TipoUsuario? Tipo { get; set; }
+    }
+}
---
+++ Entites/Entites/Notifies.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entites.Entites
+{
+    public class Notifies
+    {
+        public Notifies()
+        {
+            Notitycoes = new List<Notifies>();
+        }
+
+        [NotMapped]
+        public String NomePropriedade { get; set; }
+
+        [NotMapped]
+        public String Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifies> Notitycoes { get; set; }
+
+        public bool ValidarPropriedadeString(string valor, String nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "Campo Obrigatorio !",
+                    NomePropriedade = nomePropriedade
+                }) ;
+
+                return false;
+            }
+            return true;
+        }
+
+        public bool ValidarPropriedadeInt(int valor, String nomePropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "Campo Obrigatorio !",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+            return true;
+        }
+    }
+}
