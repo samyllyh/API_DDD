e9ba1b15c360a5590449c21288a8626f66e05fb3
samylly <samyl@DESKTOP-TRVTAH5>
2022-09-12 13:16:33 -0300

    implementação da camada de infraestrutura

---
+++ Infraestruture/Configuration/ContextBase.cs
@@ -0,0 +1,41 @@
+﻿using Entites.Entites;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestruture.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+
+        public DbSet<Message> Message { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        //string de conecção
+        public string ObterStringConexao()
+        {
+            return "Data Source=DESKTOP-TRVTAH5\\SQLEXPRESS;Initial Catalog=master;Integrated Security=True";
+        }
+
+        //se não foi configurada a string de configuração
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+
+        }
+
+        //vai mapear os usuarios pela a chave de usuario
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Infraestruture/Repository/Generics/RepositoryGenerics.cs
@@ -0,0 +1,75 @@
+﻿using Domain.Interface.Generics;
+using Infraestruture.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+
+namespace Infraestruture.Repository.Generics
+{
+    internal class RepositoryGenerics<T> : IGeneric<T>, IDisposable where T : class
+    {
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+        public RepositoryGenerics()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        public async Task Add(T objeto)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                await data.Set<T>().AddAsync(objeto); //add objeto no banco
+                await data.SaveChangesAsync(); //commit no banco
+            }
+        }
+
+        public async Task Delete(T objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetEntityById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<T>> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Update(T objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Disposed
+        bool disposedValue = false;
+
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        public void Dispose()
+        {
+            // Não altere este código. Coloque o código de limpeza no método 'Dispose(bool disposing)'
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+
+
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+        #endregion
+    }
+}
